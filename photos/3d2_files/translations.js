59c36632c18f7e841fdc8fa0b9e6bc9caa539eb0
Sighmantaneous <g00286398@atu.ie>
2023-12-15 09:27:38 +0000

    Adding updated versions of css, js and html files

---
+++ photos/3d2_files/translations.js
@@ -0,0 +1,1 @@
+window.translations_ucfirst=str=>str.charAt(0).toUpperCase()+str.slice(1).toLowerCase(),window.translations=JSON.parse('{"MANDATORY":"Mandatory","ERROR":"Error","YES":"Yes","NO":"No","ITEM":"item","ITEMS":"items","ITEMS_OF_TOTAL":"{count} of {total} items","TEMPLATE_SWITCH_CONFIRM":"Are you sure you want to switch to the desktop version?"}');
---
+++ simon.js
@@ -0,0 +1,71 @@
+function inkJet()
+    {
+        document.getElementById("threeD").style.display="none";
+        document.getElementById("laser").style.display="none";
+        document.getElementById("inkJet").style.display="initial";
+        document.getElementById("LaserPick").checked = false;
+        document.getElementById("3dPick").checked = false;
+    }
+
+function Laser()
+    {
+        document.getElementById("threeD").style.display="none";
+        document.getElementById("inkJet").style.display="none";
+        document.getElementById("laser").style.display="initial";
+        document.getElementById("3dPick").checked = false;
+        document.getElementById("InkJetPick").checked = false;
+    }
+
+function threeD()
+    {
+        document.getElementById("laser").style.display="none";
+        document.getElementById("inkJet").style.display="none";
+        document.getElementById("threeD").style.display="initial";
+        document.getElementById("LaserPick").checked = false;
+        document.getElementById("InkJetPick").checked = false;
+    }
+
+function smallPrice()
+    {
+        document.getElementById("tierM").checked = false;
+        document.getElementById("tierL").checked = false;
+        document.getElementById("inkjet3").style.visibility="hidden"
+        document.getElementById("laser1").style.visibility="hidden"
+        document.getElementById("laser2").style.visibility="hidden"
+        document.getElementById("3d1").style.visibility="hidden"
+        document.getElementById("3d2").style.visibility="hidden"
+        document.getElementById("3d3").style.visibility="hidden"
+    }
+function medPrice()
+    {
+        document.getElementById("tierS").checked = false;
+        document.getElementById("tierL").checked = false;
+        document.getElementById("laser1").style.visibility="hidden"
+        document.getElementById("inket1").style.visibility="hidden"
+        document.getElementById("inkjet2").style.visibility="hidden"
+        document.getElementById("3d2").style.visibility="hidden"
+        document.getElementById("3d3").style.visibility="hidden"
+    }
+function lrgPrice()
+    {
+        document.getElementById("tierS").checked = false;
+        document.getElementById("tierM").checked = false;
+        document.getElementById("threeD2").style.display = "none";
+        document.getElementById("laser3").style.display = "none" ;
+
+
+    }
+function resetFilter()
+    {
+        location.reload();
+    }
+function clearEmail()
+    {
+        document.getElementById('email').value = "";
+        alert("Thanks for signing up")
+    }
+
+function addToCart()
+    {
+        alert("Added to cart")
+    }
